be1a52eff365af1fe3b249d24cd500bea6c72986
Pedrovff23 <pedrovff1123@outlook.com>
2022-08-10 22:43:50 -0300

    Rota com Flask

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask("projeto")
+app = Flask(__name__)
 
 
 @app.route("/")
 def ola_mundo():
-    return "Olá Mundo! Esse é meu primeiro projeto Flask!!", 200
+    # criar uma variavel com o meu nome
+    nome = "Pedro Victor"
+    produtos = [
+        {"nome": "Ração", "preco": 199.99},
+        {"nome": "Playstation", "preco": 5000}]
+    return render_template("alo.html", produtos=produtos, nome=nome), 200
 
-
-app.run()
