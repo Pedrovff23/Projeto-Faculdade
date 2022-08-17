6b4a6e4a7e6e390192c56a40c2ddc9b95e1690fc
Pedrovff23 <Pedrovff1123@outlook.com>
2022-08-17 20:30:46 -0300

    Rotas

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -27,5 +27,23 @@ def rota_raiz():
 # Nova Rota Teste
 @app.route("/teste")
 @app.route("/teste/<variavel>")
-def rota_nova(variavel = ""):
+def rota_nova(variavel=""):
     return "Nova Rota teste<br>Variavél: {}".format(variavel), 200
+
+
+# Rota formulário
+@app.route("/form")
+def rota_formulario():
+    return render_template("form.html"), 200
+
+
+# Rota tratamento do formulario
+@app.route("/form_recebe", methods=["GET", "POST"])
+def rota_form_recebe():
+    nome = ""
+    if request.method == "POST":
+        nome = request.form["nome"]
+        return "Nome: {}".format(nome), 200
+    else:
+        return "Não pode chamer direto no GET", 200
+
