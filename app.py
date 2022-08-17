e7636db7758d267075f3ea5935f4571980d793c9
Pedrovff23 <Pedrovff1123@outlook.com>
2022-08-17 19:39:08 -0300

    nova atualização

--- app.py
+++ app.py
@@ -2,22 +2,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+produtos = [
+    {"nome": "Produto 1", "preco": "R$ ***"},
+    {"nome": "Produto 2", "preco": "R$ ***"},
+    {"nome": "Produto 3", "preco": "R$ ***"},
+    {"nome": "Produto 4", "preco": "R$ ***"},
+    {"nome": "Produto 5", "preco": "R$ ***"},
+    {"nome": "Produto 6", "preco": "R$ ***"},
+    {"nome": "Produto 7", "preco": "R$ ***"},
+    {"nome": "Produto 8", "preco": "R$ ***"},
+    {"nome": "Produto 9", "preco": "R$ ***"},
+]
 
+
+# Rota Raiz
 @app.route("/")
-def ola_mundo():
+def rota_raiz():
     # criar uma variavel com o meu nome
     nome = "Pedro Victor"
 
-    produtos = [
-        {"nome": "Produto 1", "preco": "R$ ***"},
-        {"nome": "Produto 2", "preco": "R$ ***"},
-        {"nome": "Produto 3", "preco": "R$ ***"},
-        {"nome": "Produto 4", "preco": "R$ ***"},
-        {"nome": "Produto 5", "preco": "R$ ***"},
-        {"nome": "Produto 6", "preco": "R$ ***"},
-        {"nome": "Produto 7", "preco": "R$ ***"},
-        {"nome": "Produto 8", "preco": "R$ ***"},
-        {"nome": "Produto 9", "preco": "R$ ***"},
-    ]
-
     return render_template("alo.html", produtos=produtos, nome=nome), 200
+
+
+# Nova Rota Teste
+@app.route("/teste")
+def rota_nova():
+    return "Nova Rota teste", 200
